b2d929fef8abaac4dcae5ab895419f0fd20f6bed
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2023-01-13 15:17:49 +0000

    first commit of 2023

--- tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
@@ -1,6 +1,15 @@
-module mux2_structural (output logic y, input logic a, b, s);
+module mux2_structural (output wire y, input wire a, b, s);
 
 // Write HDL here
 
+//internal wires
+wire invS;
+wire andA;
+wire andB;
 
+//place and connect gates
+not u1 (invS, s);
+and u2 (andA, a, invS);
+and u3 (andB, b, s);
+or u4 (y, andA, andB);
 endmodule
