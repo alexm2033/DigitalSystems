2062092a041e7cb698bfb1e45f6e2a2cc6df7446
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2023-01-31 10:43:24 +0000

    Update invN.sv

--- tasks/Task212-Generate/invN.sv
+++ tasks/Task212-Generate/invN.sv
@@ -1,14 +1,19 @@
 // Example of generate for to replicate hardware
-module invN #(parameter N=8) (output logic[N-1:0] Y, input logic[N-1:0] X);
+module invN #(parameter N=4) (output logic[N-1:0] Y, input logic[N-1:0] X);
 
 //An integer that will never be represented in the final synthesis
 genvar i;
+genvar j;
 
 generate 
- for (i=0; i<N; i=i+1) 
+ for (i=0; i<N; i=i+2) 
  begin : i_loop1
-	not ui (Y[i], X[i]);
+	if(i%2 == 0)
+	assign Y[i]=~X[i];
+	else
+	assign Y[i]=X[i];
  end
+ 
 endgenerate
 
 endmodule
