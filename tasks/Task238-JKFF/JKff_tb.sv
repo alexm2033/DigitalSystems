759cf6e73dc7085cb2160a43c28541e18f744c39
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2023-02-13 14:03:08 +0000

    task 238 complete

--- tasks/Task238-JKFF/JKff_tb.sv
+++ tasks/Task238-JKFF/JKff_tb.sv
@@ -1,14 +1,15 @@
 module JKff_tb;
 
-logic clk = 0;
+logic CLK = 0;
 logic Q,J,K;
 
 JKff u1(Q, J, K, CLK);
+int X;
 
 initial begin
 
 	CLK = 0;
-	repeat(10)
+	repeat(20)
 		#50ps CLK =~CLK;
 end
 
@@ -34,7 +35,19 @@ begin
 
 	@(posedge CLK);
 	{J,K} = 2'b10;
+	@(negedge CLK);
+	#1 assert (Q == 1) $display("It's gone high");
 	@(posedge CLK);
-`	{J,K} = 2'b00;
+	{J,K} = 2'b00;
 	@(negedge CLK);
 	#1 assert(Q == 1) $display("Pass latch 1"); else $error("Fail latch 1");
+
+	@(posedge CLK);
+	{J,K} = 2'b11;
+	@(negedge CLK);
+	#1 assert(Q == 0) $display("Pass toggle to 0"); else $error("Fail toggle to 0");
+	@(negedge CLK);
+	#1 assert(Q == 1) $display("Passed toggle to 1"); else $error("Fail toggle to 1");
+
+end
+endmodule
